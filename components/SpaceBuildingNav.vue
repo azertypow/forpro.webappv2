021988ae02404f99babfccd287bb13c9ed24f912
azertypow <azertypow@gmail.com>
2023-05-19 19:01:58 +0200

    feat: components and favicon

---
+++ components/SpaceBuildingNav.vue
@@ -0,0 +1,53 @@
+<template>
+    <section class="v-space-build-nav" >
+
+        <svg id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 478">
+            <g id="Layer_1-2" data-name="Layer 1">
+                <g>
+                    <rect x="975" y="0" width="166" height="69" style="fill: #d2c8b4;"/>
+                    <rect x="225" y="390" width="750" height="88" style="fill: #bea5e6;"/>
+                    <rect y="132" width="1200" height="128" style="fill: #bea5e6;"/>
+                    <rect y="61" width="1200" height="71" style="fill: #aac8ff;"/>
+                    <rect x="975" y="390" width="225" height="88" style="fill: #aac8ff;"/>
+                    <rect x="70.0027" width="905.03136" height="69" style="fill: #fffe00;"/>
+                    <rect x="975" y="260" width="225" height="130" style="fill: #3a4bcc;"/>
+                    <rect x="225" y="260" width="750" height="130" style="fill: #ff6917;"/>
+                    <rect y="260" width="225" height="130" style="fill: #ffc78c;"/>
+                    <rect y="390" width="225" height="88" style="fill: #f64a42;"/>
+                    <g>
+                        <path d="m1192,69v401H8V69h1184m8-8H0v417h1200V61h0Z" style="fill: #222221;"/>
+                        <path d="m1133,8v53H78V8h1055M1141,0H70v69h1071V0h0Z" style="fill: #222221;"/>
+                        <line y1="132" x2="1200" y2="132" style="fill: none; stroke: #222221; stroke-miterlimit: 10; stroke-width: 8px;"/>
+                        <line y1="196" x2="1200" y2="196" style="fill: none; stroke: #222221; stroke-miterlimit: 10; stroke-width: 8px;"/>
+                        <line y1="260" x2="1200" y2="260" style="fill: none; stroke: #222221; stroke-miterlimit: 10; stroke-width: 8px;"/>
+                        <line y1="390" x2="1200" y2="390" style="fill: none; stroke: #222221; stroke-miterlimit: 10; stroke-width: 8px;"/>
+                        <line y1="326" x2="1200" y2="326" style="fill: none; stroke: #222221; stroke-miterlimit: 10; stroke-width: 8px;"/>
+                        <line x1="975" y1="260" x2="975" y2="478" style="fill: none; stroke: #222221; stroke-miterlimit: 10; stroke-width: 8px;"/>
+                        <line x1="225" y1="260" x2="225" y2="478" style="fill: none; stroke: #222221; stroke-miterlimit: 10; stroke-width: 8px;"/>
+                        <line x1="975" y1="0" x2="975" y2="69" style="fill: none; stroke: #222221; stroke-miterlimit: 10; stroke-width: 8px;"/>
+                    </g>
+                </g>
+            </g>
+        </svg>
+
+    </section>
+</template>
+
+
+
+
+
+<script lang="ts" setup>
+defineProps<{
+}>()
+
+</script>
+
+
+
+
+
+<style lang="scss" scoped >
+.v-space-build-nav {
+}
+</style>
---
+++ components/TextQuote.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="v-text-quote" >
+    </section>
+</template>
+
+
+
+
+
+<script lang="ts" setup>
+defineProps<{
+}>()
+
+</script>
+
+
+
+
+
+<style lang="scss" scoped >
+.v-text-quote {
+}
+</style>
