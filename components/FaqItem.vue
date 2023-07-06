826a084f523231455c39e2b4954edad522cc7642
azertypow <azertypow@gmail.com>
2023-07-06 14:44:35 +0200

    feat: F.A.Q. update

--- components/FaqItem.vue
+++ components/FaqItem.vue
@@ -6,15 +6,19 @@
         class="v-faq-item">
         <div
             @click="responseIsOpen = !responseIsOpen"
-            class="v-faq-item__icon">
-            <div class="v-faq-item__icon__horizontal" ></div>
-            <div class="v-faq-item__icon__vertical" ></div>
-        </div>
-        <div>
+            class="v-faq-item__header"
+        >
+            <div
+                class="v-faq-item__icon">
+                <div class="v-faq-item__icon__horizontal" ></div>
+                <div class="v-faq-item__icon__vertical" ></div>
+            </div>
             <h5
                 class="v-faq-item__question fp-text--without-margin">
                 {{question}}
             </h5>
+        </div>
+        <div>
             <Transition>
             <p
                 v-if="responseIsOpen"
@@ -64,22 +68,38 @@ const responseIsOpen = ref(false)
 .v-faq-item {
     --button-width: 1rem;
     --button-width-half: calc( var(--button-width) / 2 );
-    --line-width: 3px;
+    --line-width: 2px;
 
     position: relative;
     padding-left: calc( var(--button-width) + var(--fp-gutter) );
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    border-bottom: solid 1px;
+
+    &:first-child {
+        border-top: solid 1px;
+    }
 
     > * {
         width: 100%;
     }
 }
 
+.v-faq-item__header {
+    position: relative;
+    cursor: pointer;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    color: var(--fp-theme-color-secondary);
+}
+
 .v-faq-item__icon {
     position: absolute;
-    top: 0;
-    left: 0;
+    top: 50%;
+    right: calc( 100% + var(--fp-gutter) );
+    transform: translate(0, -50%);
     width: var(--button-width);
-    height: 100%;
+    height: var(--button-width);
     cursor: pointer;
     //background: red;
     flex-shrink: 0;
@@ -104,11 +124,11 @@ const responseIsOpen = ref(false)
 
 .v-faq-item__icon__vertical {
     transition: transform ease-in-out .5s .25s;
-    transform: scale(1, 1);
+    transform: rotate(0);
 
     .response-is-open & {
         transition: transform ease-in-out .25s;
-        transform: scale(1, 0);
+        transform: rotate(90deg);
     }
 }
 
--- pages/faq.vue
+++ pages/faq.vue
@@ -30,34 +30,38 @@
                 class="fp-grid-coll-14-24 fp-grid-with-gutter fp-page__sections v-faq__content"
             >
                 <h3 id="LearningLab" >LearningLab</h3>
-                <faq-item
-                    response="Amet aut consequuntur cupiditate delectus exercitationem facere magnam, maiores nisi, non omnis sunt totam veritatis vitae voluptatibus voluptatum. Accusantium adipisci illo temporibus!"
-                    question="Amet at corporis facere hic ipsa mollitia necessitatibus optio quae vitae. Aliquam beatae eius error maiores? Cupiditate error excepturi explicabo quod vel!"
-                    link=""/>
-                <faq-item
-                    response="Architecto aut culpa cum dolor dolore dolorem ducimus et fugiat id illo in, inventore magnam necessitatibus, optio quaerat quidem quisquam rerum tempore."
-                    question="Ab beatae eos odio! Cumque, officia voluptate. Aut eum impedit iusto. Earum fugiat ipsa odit sequi tempora. Aliquid asperiores dolore illo iure?"
-                    link=""/>
-                <faq-item
-                    response="Aliquid at cum, cumque cupiditate delectus earum expedita illo impedit ipsum iste iusto nostrum provident quae quod repellendus repudiandae sequi vitae voluptas?"
-                    question="Alias aut, consectetur eligendi exercitationem libero magnam molestias, nemo nesciunt numquam pariatur provident quae quibusdam rem sequi sint sit tenetur ut voluptates!"
-                    link=""/>
-                <faq-item
-                    response="Ab architecto dicta esse itaque nemo perferendis quaerat qui soluta vero! Accusamus blanditiis consequuntur culpa dolor doloremque eligendi ex magnam magni, nulla?"
-                    question="Autem dolor esse, facere libero nostrum obcaecati provident repudiandae. Amet commodi cumque earum inventore quasi repellendus, sint tempora. Consequuntur mollitia nulla tempore!"
-                    link=""/>
-                <faq-item
-                    response="A architecto dolores error fugit id ipsa laboriosam, laudantium necessitatibus obcaecati, placeat quis reiciendis repudiandae vero? Cupiditate dolor harum in omnis vero!"
-                    question="Amet doloribus enim laboriosam nam nulla reiciendis sit, suscipit temporibus veritatis! Consectetur ea eius iure labore magnam modi repellat reprehenderit rerum vitae."
-                    link=""/>
-                <faq-item
-                    response="Adipisci animi beatae culpa dignissimos error esse, inventore magni maxime mollitia nulla placeat porro sed sequi soluta sunt tenetur voluptatem! Architecto, similique!"
-                    question="Fugit in ipsa iusto officia provident quibusdam tenetur? Adipisci, dolore earum enim eum facere quae quisquam, repellat rerum sed sit vitae voluptatem."
-                    link=""/>
-                <faq-item
-                    response="Delectus doloribus ducimus et optio sunt? Consectetur cumque cupiditate dolore harum inventore quia recusandae tempora? Ad amet incidunt ipsam optio, rerum sapiente."
-                    question="Blanditiis dignissimos doloremque dolorum eius fugiat hic, id minima nesciunt nihil praesentium repellat repudiandae rerum temporibus voluptatem voluptates. Doloremque exercitationem ipsa possimus."
-                    link=""/>
+                <div
+                    class="v-faq__content__questions"
+                >
+                    <faq-item
+                        response="Amet aut consequuntur cupiditate delectus exercitationem facere magnam, maiores nisi, non omnis sunt totam veritatis vitae voluptatibus voluptatum. Accusantium adipisci illo temporibus!"
+                        question="Amet at corporis facere hic ipsa mollitia necessitatibus optio quae vitae. Aliquam beatae eius error maiores? Cupiditate error excepturi explicabo quod vel!"
+                        link=""/>
+                    <faq-item
+                        response="Architecto aut culpa cum dolor dolore dolorem ducimus et fugiat id illo in, inventore magnam necessitatibus, optio quaerat quidem quisquam rerum tempore."
+                        question="Ab beatae eos odio! Cumque, officia voluptate. Aut eum impedit iusto. Earum fugiat ipsa odit sequi tempora. Aliquid asperiores dolore illo iure?"
+                        link=""/>
+                    <faq-item
+                        response="Aliquid at cum, cumque cupiditate delectus earum expedita illo impedit ipsum iste iusto nostrum provident quae quod repellendus repudiandae sequi vitae voluptas?"
+                        question="Alias aut, consectetur eligendi exercitationem libero magnam molestias, nemo nesciunt numquam pariatur provident quae quibusdam rem sequi sint sit tenetur ut voluptates!"
+                        link=""/>
+                    <faq-item
+                        response="Ab architecto dicta esse itaque nemo perferendis quaerat qui soluta vero! Accusamus blanditiis consequuntur culpa dolor doloremque eligendi ex magnam magni, nulla?"
+                        question="Autem dolor esse, facere libero nostrum obcaecati provident repudiandae. Amet commodi cumque earum inventore quasi repellendus, sint tempora. Consequuntur mollitia nulla tempore!"
+                        link=""/>
+                    <faq-item
+                        response="A architecto dolores error fugit id ipsa laboriosam, laudantium necessitatibus obcaecati, placeat quis reiciendis repudiandae vero? Cupiditate dolor harum in omnis vero!"
+                        question="Amet doloribus enim laboriosam nam nulla reiciendis sit, suscipit temporibus veritatis! Consectetur ea eius iure labore magnam modi repellat reprehenderit rerum vitae."
+                        link=""/>
+                    <faq-item
+                        response="Adipisci animi beatae culpa dignissimos error esse, inventore magni maxime mollitia nulla placeat porro sed sequi soluta sunt tenetur voluptatem! Architecto, similique!"
+                        question="Fugit in ipsa iusto officia provident quibusdam tenetur? Adipisci, dolore earum enim eum facere quae quisquam, repellat rerum sed sit vitae voluptatem."
+                        link=""/>
+                    <faq-item
+                        response="Delectus doloribus ducimus et optio sunt? Consectetur cumque cupiditate dolore harum inventore quia recusandae tempora? Ad amet incidunt ipsam optio, rerum sapiente."
+                        question="Blanditiis dignissimos doloremque dolorum eius fugiat hic, id minima nesciunt nihil praesentium repellat repudiandae rerum temporibus voluptatem voluptates. Doloremque exercitationem ipsa possimus."
+                        link=""/>
+                </div>
 
                 <h3 id="MakerLab" >MakerLab</h3>
                 <faq-item
@@ -304,9 +308,10 @@ onMounted(() => {
 }
 
 .v-faq__content {
-    > *:not(h3) {
-        margin-top: 2rem;
-    }
+}
+
+.v-faq__content__questions {
+
 }
 
 .v-faq__nav {
@@ -317,6 +322,15 @@ onMounted(() => {
         position: absolute;
         top: 0;
         left: var(--fp-gutter);
+
+        a,
+        a.is-active {
+            color: inherit;
+
+            &:hover {
+                color: var(--fp-theme-color-main);
+            }
+        }
     }
 }
 </style>
