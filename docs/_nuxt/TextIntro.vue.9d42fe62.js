84beca2e8d80302002b8f4261765092268930e5b
azertypow <azertypow@gmail.com>
2023-07-31 11:47:30 +0200

    build

---
+++ docs/_nuxt/TextIntro.vue.9d42fe62.js
@@ -0,0 +1,1 @@
+import{a as t,o as n,i as o}from"./entry.67fe5d42.js";const r=["innerHTML"],i=t({__name:"TextIntro",props:{content:{type:String,required:!0}},setup(e){return(_,s)=>(n(),o("section",{class:"v-text-intro",innerHTML:e.content},null,8,r))}});export{i as _};
