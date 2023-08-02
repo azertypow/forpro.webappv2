6e457410a6c38ecc8952f7d89389e4edc4010137
azertypow <azertypow@gmail.com>
2023-08-02 15:10:30 +0200

    feat: - update color for plan - update xl plan - plan navigation - plan over interraction - plan content - article cotent for title - error page - last news component init - home image header import and style

---
+++ components/LastNews.vue
@@ -0,0 +1,34 @@
+<template>
+    <section class="v-last-news" >
+        {{lastBlogArticles.data}}
+<!--        <div-->
+<!--            v-if="lastBlogArticles.data.value"-->
+<!--        >-->
+
+<!--            {{lastBlogArticles.data.value.lastArticle.title}}-->
+<!--        </div>-->
+    </section>
+</template>
+
+
+
+
+
+<script lang="ts" setup>
+import {fetchForProApi_blogLastArticles} from "~/global/forProApi"
+
+// defineProps<{
+// }>()
+
+const lastBlogArticles = await fetchForProApi_blogLastArticles()
+
+</script>
+
+
+
+
+
+<style lang="scss" scoped >
+.v-last-news {
+}
+</style>
