b33d2179ae240135c7638b37f35d9e15bdab3f0a
azertypow <azertypow@gmail.com>
2023-08-08 10:33:42 +0200

    feat: Rob retours

---
+++ pages/contact.vue
@@ -0,0 +1,93 @@
+<template>
+    <main class="v-contact fp-page fp-page--no-margin-bottom" >
+        <page-header
+            page-title="contact"
+            :is-home="true"
+        />
+
+        <div
+            class="fp-grid-coll-container fp-grid-coll-container--center"
+        >
+            <div
+                class="fp-grid-coll-24-24 fp-grid-coll-reg-16-24 fp-grid-with-gutter"
+            >
+                <h3>Adresse</h3>
+                <h3>Comment venir</h3>
+                <h3>Google maps</h3>
+            </div>
+        </div>
+    </main>
+
+</template>
+
+
+
+
+
+<style lang="scss" scoped>
+@use "assets/_scss-params";
+
+
+.v-contact {
+    min-height: 100vh;
+    color: var(--fp-color-black);
+    background-color: var(--fp-color-white);
+    position: relative;
+}
+
+</style>
+
+
+
+
+
+<script lang="ts" setup >
+import {useAppStateStore} from "~/stores/appState"
+import {onMounted, useError} from "#imports"
+import {setForProSiteData} from "~/global/appDataSeters";
+
+defineProps<{
+    error: Error
+}>()
+
+const stateLoading = ref(false)
+const stateIsLoaded = ref(false)
+
+onMounted(() => {
+
+    setForProSiteData()
+    useAppStateStore().updateTheme('white_pink_blue')
+
+    window.addEventListener('scroll', () => {
+        useAppStateStore().updateBackgroundColor()
+    })
+
+    useAppStateStore().updateBackgroundColor()
+
+    nextTick(() => {
+        stateLoading.value = true
+        setTimeout(() => {
+            stateLoading.value = false
+            stateIsLoaded.value = true
+        }, 1_000)
+    })
+})
+
+const error = useError().value
+
+function isObjectWithStatusCode(value: any): value is {
+    url: string;
+    statusCode: number;
+    statusMessage: string;
+    message: string;
+    description: string;
+    data?: any;
+} {
+    return value.hasOwnProperty('statusCode')
+}
+
+function backToHome() {
+    clearError({redirect: '/'})
+}
+
+</script>
---
+++ pages/impressum.vue
@@ -0,0 +1,95 @@
+<template>
+    <main class="v-impressum-name fp-page" >
+        <header
+            class="v-impressum-name__header fp-grid-with-gutter fp-remove-margin-child"
+        >
+            <h1 class="fp-heading-h2" >Impressum</h1>
+        </header>
+
+
+        <div
+            class="fp-page__sections-box"
+        >
+            <div>
+                <div
+                    class="fp-page__sections-box__section fp-page__sections-box__section--is-heading fp-grid-coll-container fp-grid-coll-container--center"
+                >
+                    <div class="fp-grid-coll-24-24 fp-grid-coll-reg-18-24">
+                        <text-content
+                            content="<h4>Platforme web</h4>"
+                        />
+                    </div>
+                </div>
+
+                <div
+                    class="fp-page__sections-box__section fp-grid-coll-container fp-grid-coll-container--center"
+                >
+                    <div class="fp-grid-coll-24-24 fp-grid-coll-reg-18-24 fp-grid-with-gutter">
+                        <text-content
+                            content="<h6>
+                            Images du bâtiment et portraits d'apprenti·e·s: Estelle Bouchet et Alicia Dubuis
+                            <br>Développement: Nicolas Baldran
+                            <br>Webdesign: Sonia Dominguez & Rob van Leijsen
+                            <br>Photographie: Raphaëlle Mueller
+
+                            </h6>"
+                        />
+                    </div>
+                </div>
+
+                <div
+                    class="fp-page__sections-box__section fp-page__sections-box__section--is-heading fp-grid-coll-container fp-grid-coll-container--center"
+                >
+                    <div class="fp-grid-coll-24-24 fp-grid-coll-reg-18-24">
+                        <text-content
+                            content="<h4>Identité visuelle et signaletique</h4>"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </main>
+</template>
+
+
+
+
+
+<script lang="ts" setup>
+
+</script>
+
+
+
+
+
+<style lang="scss" scoped >
+.v-impressum-name {
+    padding-bottom: 5rem;
+}
+
+.v-impressum-name__header {
+    padding-top:    2.5rem;
+    text-align: center;
+    color: var(--fp-theme-color-main);
+
+    > * {
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    .v-impressum-name__img {
+        padding-bottom: calc( 100% / 12 * 7 );
+        background-color: var(--fp-color-grey);
+        background-size: cover;
+        background-position: center center;
+        margin-bottom: .5rem;
+    }
+}
+
+.v-impressum-name__date {
+    color: var(--fp-theme-color-secondary);
+    margin-bottom: 1rem;
+}
+</style>
