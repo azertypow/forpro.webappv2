cb2cebd5457daf0624fe8a8c5b2814ff935a8b6d
azertypow <azertypow@gmail.com>
2023-08-17 12:35:35 +0200

    fix: bullet point from div tu ul>li

--- components/TextBulletPoint.vue
+++ components/TextBulletPoint.vue
@@ -22,40 +22,44 @@ defineProps<{
 
 <style lang="scss"  >
 .v-text-bullet-point {
-  display: flex;
-    flex-wrap: wrap;
-    gap: 1rem;
 
-  > li
-  > ul {
-      box-sizing: border-box;
-      padding-left: 2rem;
-      position: relative;
-      width: 100%;
+    ul {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 1rem;
+    }
 
-      @media (min-width: 1000px) {
-        width: calc(50% - .5rem);
-      }
+    li {
+        box-sizing: border-box;
+        padding-left: 2rem;
+        position: relative;
+        width: 100%;
 
-      &:before {
-          content: '';
-          position: absolute;
-          top: 0;
-          left: 0;
-          display: block;
-          background-image: url('../assets/SVGs/v-text-bullet-point--01.svg');
-          width:    1.5rem;
-          height:   1.5rem;
-          background-size: contain;
-          background-repeat: no-repeat;
-          background-position: right center;
-      }
-      &:nth-child( 1n ):before {
-          background-image: url('../assets/SVGs/v-text-bullet-point--01.svg');
-      }
-      &:nth-child( 2n ):before {
-          background-image: url('../assets/SVGs/v-text-bullet-point--06.svg');
-      }
-  }
+        @media (min-width: 1000px) {
+            width: calc(50% - .5rem);
+        }
+
+        &:before {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            display: block;
+            background-image: url('../assets/SVGs/v-text-bullet-point--01.svg');
+            width: 1.5rem;
+            height: 1.5rem;
+            background-size: contain;
+            background-repeat: no-repeat;
+            background-position: right center;
+        }
+
+        &:nth-child( 1n ):before {
+            background-image: url('../assets/SVGs/v-text-bullet-point--01.svg');
+        }
+
+        &:nth-child( 2n ):before {
+            background-image: url('../assets/SVGs/v-text-bullet-point--06.svg');
+        }
+    }
 }
 </style>
