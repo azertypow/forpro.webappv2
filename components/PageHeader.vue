00588fef49885a13d52a7a55a606608399698960
azertypow <azertypow@gmail.com>
2023-08-28 17:44:53 +0200

    feat: init two svg animations

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -15,7 +15,7 @@
               }"
       >
           <svg
-                  class="v-page-header__graphique-image"
+                  class="v-page-header__graphique-image v-page-header__graphique-image--left"
                   viewBox="0 0 4310.6665 1991.5146"
                   xmlns="http://www.w3.org/2000/svg"
                   ref="graphicToMove"
@@ -29,6 +29,24 @@
                   />
               </g>
           </svg>
+
+
+          <svg
+                  class="v-page-header__graphique-image v-page-header__graphique-image--right"
+                  viewBox="0 0 4310.6665 1991.5146"
+                  xmlns="http://www.w3.org/2000/svg"
+                  ref="graphicToMove"
+          >
+              <g
+                      transform="matrix(1.3333333,0,0,-1.3333333,0,4000)"
+              >
+                  <path
+                          d="m 0,1623.1341 v 1260.096 c 0,84.069 38.732,116.77 122.24197,116.77 H 1703.679 c 149.938,-47.145 382.746,-214.095 519.991,-372.894 179.769,-207.998 377.718,-278.564 668.365,-278.564 H 3203 c 16.568,0 30,-13.431 30,-30 v -130.72 c 0,-16.569 -13.432,-30 -30,-30 h -310.965 c -199.5435,3.2233 -488.596,-70.566 -668.365,-278.564 -137.245,-158.799 -257.798,-372.894 -519.991,-372.894 H 122.24197 C 38.732,1506.3641 0,1539.0651 0,1623.1341"
+                          style="fill:#f500b4;fill-opacity:1;fill-rule:nonzero;stroke:none"
+                  />
+              </g>
+          </svg>
+
       </div>
       <div
               class="v-page-header__title fp-grid-with-gutter fp-remove-margin-child"
@@ -89,20 +107,22 @@ onUnmounted(() => {
 const onScrollCallback = () => {
   if( ! (headerMain.value instanceof HTMLElement) ) return
 
-  const headerGraphicImage = document.querySelector('.v-page-header__graphique-image')
+  const nodeListOfHeaderGraphicImage = document.querySelectorAll('.v-page-header__graphique-image')
 
-  if( ! (headerGraphicImage instanceof SVGElement) ) return
+    nodeListOfHeaderGraphicImage.forEach((headerGraphicImage: Element) => {
+            if( ! (headerGraphicImage instanceof SVGElement) ) return
 
-  const YTranslation = map(
-    headerMain.value.getBoundingClientRect().height + headerMain.value.getBoundingClientRect().top,
-    0,
-    headerMain.value.getBoundingClientRect().height,
-    0,
-    100,
-  )
+            const YTranslation = map(
+                headerMain.value!.getBoundingClientRect().height + headerMain.value!.getBoundingClientRect().top,
+                0,
+                headerMain.value!.getBoundingClientRect().height,
+                0,
+                100,
+            )
 
-  headerGraphicImage.style.setProperty('--scroll-percent', (YTranslation / 100).toString())
-
+            headerGraphicImage.style.setProperty('--scroll-percent', (YTranslation / 100).toString())
+        }
+    )
 }
 
 const map = (
@@ -150,26 +170,44 @@ const map = (
   --scroll-percent: 100;
 
   display: block;
-  height: 100vh;
   position: absolute;
-  bottom: -50%;
-  right: 50%;
 
   path {
     fill: var(--fp-theme-color-secondary) !important;
   }
 
-  // animation
-  animation: header-animation 1s ease-in-out infinite;
-  animation-delay: calc(var(--scroll-percent) * -1s);
-  animation-fill-mode: both;
-  animation-iteration-count: 1;
-  animation-play-state: paused;
-  //animation-iteration-count: infinite;
-  //animation-play-state: running;
+  &.v-page-header__graphique-image--left {
+    height: 100vh;
+    bottom: -50%;
+    right: 50%;
+
+    // animation
+    animation: header-animation--left 1s ease-in-out infinite;
+    animation-delay: calc(var(--scroll-percent) * -1s);
+    animation-fill-mode: both;
+    animation-iteration-count: 1;
+    animation-play-state: paused;
+    //animation-iteration-count: infinite;
+    //animation-play-state: running;
+  }
+
+  &.v-page-header__graphique-image--right {
+    height: 100vh;
+    bottom: -50%;
+    left: 100%;
+
+    // animation
+    animation: header-animation--right 1s ease-in-out infinite;
+    animation-delay: calc(var(--scroll-percent) * -1s);
+    animation-fill-mode: both;
+    animation-iteration-count: 1;
+    animation-play-state: paused;
+    //animation-iteration-count: infinite;
+    //animation-play-state: running;
+  }
 }
 
-@keyframes header-animation {
+@keyframes header-animation--left {
   0% {
     transform: translate(0rem, 15vh) rotate(-10deg);
   }
@@ -186,6 +224,30 @@ const map = (
     transform: translate(0, 6.4vh) rotate(0deg);
   }
 }
+
+@keyframes header-animation--right {
+  0% {
+      transform: translate(0, 50vh) rotate(15deg);
+  }
+
+  50% {
+    transform: translate(-10vw, 100vh) rotate(15deg);
+  }
+
+  100% {
+    transform: translate(0, 0) rotate(0deg);
+  }
+}
+
+
+
+
+
+
+
+
+
+
 
 .v-page-header__home {
     position: relative;
