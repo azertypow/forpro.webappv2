75ff0cd462fb6a44d754b086537db6dbfd7a8876
azertypow <azertypow@gmail.com>
2023-11-10 00:21:56 +0100

    feat: recrutement page

---
+++ components/TimeLine.vue
@@ -0,0 +1,170 @@
+<template>
+    <section class="v-time-line" >
+        <div
+                class="v-time-line__item"
+        >
+            <h6 class="v-time-line__item__date"   >01.12.2023</h6>
+            <h3 class="v-time-line__item__title"  >Ouverture des candidatures</h3>
+            <p  class="v-time-line__item__desc"   >Ouverture des postes – site internet et LinkedIn</p>
+        </div>
+
+        <div
+                class="v-time-line__item"
+        >
+            <h6 class="v-time-line__item__date"   >01.12.2023 au 10.1.2024</h6>
+            <h3 class="v-time-line__item__title"  >Délais de postulation pour déposer votre candidature</h3>
+            <p  class="v-time-line__item__desc"   ><a target="_blank" href="#" >Comment déposer ma candidature</a></p>
+        </div>
+
+
+        <div
+                class="v-time-line__item"
+        >
+            <h6 class="v-time-line__item__date"   >19.1.2024</h6>
+            <h3 class="v-time-line__item__title"  >Sélection 1<sup>er</sup> tour sur dossier</h3>
+            <p  class="v-time-line__item__desc"   >Mail d’annonce aux candidat·es sélectionné·es au 1er tour</p>
+        </div>
+        <div
+                class="v-time-line__item"
+        >
+            <h6 class="v-time-line__item__date"   >10.2.2024</h6>
+            <h3 class="v-time-line__item__title"  >1<sup>er</sup> tour, Atelier collectif</h3>
+            <p  class="v-time-line__item__desc"   ><a href="#">Atelier collectif de recrutement</a><br/>Horaires : 8h30 à 13h (obligatoire)</p>
+        </div>
+        <div
+                class="v-time-line__item"
+        >
+            <h6 class="v-time-line__item__date"   >13.2.2024 au 16.2.2024</h6>
+            <h3 class="v-time-line__item__title"  >2<sup>ème</sup> tour FoodLab</h3>
+            <p  class="v-time-line__item__desc"   >Entretiens individuels</p>
+        </div>
+        <div
+                class="v-time-line__item"
+        >
+            <h6 class="v-time-line__item__date"   >26.2.2024</h6>
+            <h3 class="v-time-line__item__title"  >Décisions FoodLab</h3>
+            <p  class="v-time-line__item__desc"   >Appels téléphoniques pour informer les candidat·es</p>
+        </div>
+        <div
+                class="v-time-line__item"
+        >
+            <h6 class="v-time-line__item__date"   >4.3.2024 au 8.3.2024</h6>
+            <h3 class="v-time-line__item__title"  >2<sup>ème</sup> tour LearningLab</h3>
+            <p  class="v-time-line__item__desc"   >Entretiens individuels</p>
+        </div>
+        <div
+                class="v-time-line__item"
+        >
+            <h6 class="v-time-line__item__date"   >15.3.2024</h6>
+            <h3 class="v-time-line__item__title"  >Décisions LearningLab</h3>
+            <p  class="v-time-line__item__desc"   >Appels téléphoniques pour informer les candidat·es</p>
+        </div>
+        <div
+                class="v-time-line__item"
+        >
+            <h6 class="v-time-line__item__date"   >1.6.2024 ou à convenir</h6>
+            <h3 class="v-time-line__item__title"  >Entrée en fonction FoodLab</h3>
+        </div>
+        <div
+                class="v-time-line__item"
+        >
+            <h6 class="v-time-line__item__date"   >1.7.2024 ou à convenir</h6>
+            <h3 class="v-time-line__item__title"  >Entrée en fonction LearningLab</h3>
+        </div>
+    </section>
+</template>
+
+
+
+
+
+<script lang="ts" setup>
+// defineProps<{
+// }>()
+
+
+</script>
+
+
+
+
+
+<style lang="scss" scoped >
+.v-time-line {
+  position: relative;
+  display: block;
+
+  &:before {
+    content: '';
+    position: absolute;
+    width: 4px;
+    height: 100%;
+    background: var(--fp-theme-color-secondary);
+    top: 0;
+    left: 50%;
+    transform: translate(-50%, 0);
+  }
+
+  .v-time-line__item {
+    position: relative;
+    width: 50%;
+    box-sizing: border-box;
+    padding-right: 2rem;
+    margin-top: -1.4rem;
+
+    &:before {
+      content: "";
+      position: absolute;
+      display: block;
+      width: 1.5rem;
+      height: 1.5rem;
+      box-sizing: border-box;
+      background: #ffcb8f;
+      border: solid 3px var(--fp-theme-color-secondary);
+      border-radius: 100%;
+      right: 0;
+      top: 0;
+      transform: translate(50%, -25%);
+    }
+
+    &:first-child {
+      margin-top: 0;
+    }
+
+    &:nth-child(2n) {
+      margin-left: 50%;
+      padding-right: 0;
+      padding-left: 2rem;
+
+      &:before {
+        right: auto;
+        left: 0;
+        top: 0;
+        transform: translate(-50%, -25%);
+      }
+    }
+
+    .v-time-line__item__date {
+      color: var(--fp-theme-color-secondary);
+    }
+
+    .v-time-line__item__title {
+      margin: 0;
+      text-align: left;
+    }
+
+    .v-time-line__item__desc {
+      margin: 0;
+      text-align: left;
+      padding-top: 1rem;
+
+      a {
+        color: inherit;
+        text-decoration: underline;
+      }
+    }
+  }
+}
+
+
+</style>
---
+++ pages/recrutement.vue
@@ -0,0 +1,185 @@
+<template>
+    <main class="v-recrutement fp-page" >
+        <page-header
+            page-title="Recrutement"
+        />
+
+        <div
+            class="fp-grid-coll-container fp-grid-coll-container--center"
+        >
+            <div
+                class="fp-grid-coll-24-24 fp-grid-coll-reg-18-24 fp-grid-with-gutter"
+            >
+                <text-intro
+                    content="
+                    <p>Nous recrutons pour l’été 2024 des professionnel.es qui ont une forte
+                    appétence pour le public jeune, pour le travail collaboratif, qui apprécient
+                    un environnement toujours en mouvement et qui souhaitent faire partie
+                    de&nbsp;l’aventure des LABs&nbsp;ForPro&nbsp;!</p>"
+                />
+            </div>
+        </div>
+
+        <div
+            class="fp-page__sections-box"
+        >
+            <div
+                class="fp-grid-coll-container fp-grid-coll-container--center fp-page__sections-box__section"
+            >
+                <div
+                    class="fp-grid-coll-24-24 fp-grid-coll-reg-18-24 fp-grid-with-gutter fp-remove-margin-child"
+                >
+                    <iframe
+                        type="text/html"
+                        width="720"
+                        height="405"
+                        src="https://www.youtube.com/embed/K8hkcHexRh4?controls=0&color=white&rel=0"
+                        frameborder="0" allowfullscreen
+                    />
+                    <iframe
+                        type="text/html"
+                        width="720"
+                        height="405"
+                        src="https://www.youtube.com/embed/IbjWlD3urr4?controls=0&color=white&rel=0"
+                        frameborder="0" allowfullscreen
+                    />
+                    <iframe
+                        type="text/html"
+                        width="720"
+                        height="405"
+                        src="https://www.youtube.com/embed/6jGrEwKLNvI?controls=0&color=white&rel=0"
+                        frameborder="0" allowfullscreen
+                    />
+                </div>
+            </div>
+
+            <div
+                class="fp-grid-coll-container fp-grid-coll-container--center fp-page__sections-box__section"
+            >
+                <div
+                    class="fp-grid-coll-24-24 fp-grid-coll-reg-18-24 fp-grid-with-gutter"
+                >
+                    <h2 style="margin-top: 2rem; margin-bottom: 4rem;">Processus de recrutement</h2>
+                </div>
+                <div
+                    class="fp-grid-coll-24-24 fp-grid-coll-reg-18-24 fp-grid-with-gutter"
+                >
+                    <time-line/>
+                </div>
+            </div>
+
+            <div
+                class="fp-grid-coll-container fp-grid-coll-container--center fp-page__sections-box__section"
+                style="margin-bottom: 5rem"
+            >
+                <div
+                    class="fp-grid-coll-24-24 fp-grid-coll-reg-18-24 fp-grid-with-gutter"
+                >
+                    <h2 style="margin-top: 2rem; margin-bottom: 4rem;">Documents</h2>
+                </div>
+                <div
+                    class="fp-grid-coll-24-24 fp-grid-coll-reg-18-24 fp-grid-with-gutter"
+                >
+                    <a
+                        class="v-recrutement__pdf-item"
+                        href="#"
+                        target="_blank"
+                    >
+                        <span>Titre du document pour le recrutement exemple</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M360-460h40v-80h40q17 0 28.5-11.5T480-580v-40q0-17-11.5-28.5T440-660h-80v200Zm40-120v-40h40v40h-40Zm120 120h80q17 0 28.5-11.5T640-500v-120q0-17-11.5-28.5T600-660h-80v200Zm40-40v-120h40v120h-40Zm120 40h40v-80h40v-40h-40v-40h40v-40h-80v200ZM320-240q-33 0-56.5-23.5T240-320v-480q0-33 23.5-56.5T320-880h480q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H320Zm0-80h480v-480H320v480ZM160-80q-33 0-56.5-23.5T80-160v-560h80v560h560v80H160Zm160-720v480-480Z"/></svg>
+                    </a>
+                    <a
+                        class="v-recrutement__pdf-item"
+                        href="#"
+                        target="_blank"
+                    >
+                        <span>Titre du document pour le recrutement exemple</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M360-460h40v-80h40q17 0 28.5-11.5T480-580v-40q0-17-11.5-28.5T440-660h-80v200Zm40-120v-40h40v40h-40Zm120 120h80q17 0 28.5-11.5T640-500v-120q0-17-11.5-28.5T600-660h-80v200Zm40-40v-120h40v120h-40Zm120 40h40v-80h40v-40h-40v-40h40v-40h-80v200ZM320-240q-33 0-56.5-23.5T240-320v-480q0-33 23.5-56.5T320-880h480q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H320Zm0-80h480v-480H320v480ZM160-80q-33 0-56.5-23.5T80-160v-560h80v560h560v80H160Zm160-720v480-480Z"/></svg>
+                    </a>
+                    <a
+                        class="v-recrutement__pdf-item"
+                        href="#"
+                        target="_blank"
+                    >
+                        <span>Titre du document pour le recrutement exemple</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M360-460h40v-80h40q17 0 28.5-11.5T480-580v-40q0-17-11.5-28.5T440-660h-80v200Zm40-120v-40h40v40h-40Zm120 120h80q17 0 28.5-11.5T640-500v-120q0-17-11.5-28.5T600-660h-80v200Zm40-40v-120h40v120h-40Zm120 40h40v-80h40v-40h-40v-40h40v-40h-80v200ZM320-240q-33 0-56.5-23.5T240-320v-480q0-33 23.5-56.5T320-880h480q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H320Zm0-80h480v-480H320v480ZM160-80q-33 0-56.5-23.5T80-160v-560h80v560h560v80H160Zm160-720v480-480Z"/></svg>
+                    </a>
+                </div>
+            </div>
+
+        </div>
+
+
+    </main>
+
+</template>
+
+
+
+
+
+<script lang="ts" setup>
+import {useAppStateStore} from "~/stores/appState";
+import TimeLine from "~/components/TimeLine.vue";
+
+// todo: redirecction if page is not active
+// throw createError({
+//     statusCode: 404,
+//     statusMessage: 'Page Not Found',
+//     fatal: true,
+// })
+
+useAppStateStore().updateTheme('fp-var-theme-color-default')
+
+
+
+</script>
+
+
+
+
+
+<style lang="scss" scoped >
+.v-recrutement {
+
+    iframe {
+        display: block;
+        margin: 0 auto 4rem;
+    }
+}
+
+.v-recrutement__pdf-item {
+    box-sizing: border-box;
+    border: solid 2px;
+    padding: 1rem;
+    border-radius: 1rem;
+    display: flex;
+    justify-content: space-between;
+    cursor: pointer;
+    align-items: flex-start;
+
+    span {
+        display: block;
+        box-sizing: border-box;
+        padding-right: 1rem;
+    }
+
+    svg {
+        fill: var(--fp-theme-color-secondary);
+        height: 1rem;
+        width: auto;
+    }
+
+    &:hover {
+        color: white;
+        background: var(--fp-theme-color-secondary);
+        svg {
+            fill: white;
+        }
+    }
+
+    & + & {
+        margin-top: 1rem;
+    }
+}
+</style>
