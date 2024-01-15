f0d53a3e5defad140747b216b74a6520f045f848
azertypow <azertypow@gmail.com>
2024-01-15 20:10:41 +0100

    build

---
+++ docs/_nuxt/TextContent.vue.475d3de1.js
@@ -0,0 +1,1 @@
+import{d as t,b as n,c as o}from"./entry.d4cccab8.js";const c=["innerHTML"],i=t({__name:"TextContent",props:{content:{required:!0,type:String}},setup(e){return(r,_)=>(n(),o("section",{class:"v-text-content fp-remove-margin-child",innerHTML:e.content},null,8,c))}});export{i as _};
